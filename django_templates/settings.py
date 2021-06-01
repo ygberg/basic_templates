12c8dfb6d9c8fd216fe8de1d89cc692be947cc5e
ygberg <fredrik.ygberg@gmail.com>
2021-06-01 15:30:42 +0200

    image update and container for paragraphs

--- django_templates/settings.py
+++ django_templates/settings.py
@@ -119,3 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_DIR = (
+    os.path.join(BASE_DIR,'media')
+)
+STATICFILES_DIRS = [
+    STATIC_DIR,
+]
